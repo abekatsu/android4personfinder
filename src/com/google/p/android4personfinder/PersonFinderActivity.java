94b66af8878ed7a5548ca71e5207512b9959e576
Katsuhisa ABE <katsuhisa.abe@gmail.com>
2011-03-21 09:59:58 +0900

    Add edittext preferences for alternate family name and alternate given name.

--- src/com/google/p/android4personfinder/PersonFinderActivity.java
+++ src/com/google/p/android4personfinder/PersonFinderActivity.java
@@ -1,24 +1,21 @@
 package com.google.p.android4personfinder;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class PersonFinderActivity extends Activity {
+public class PersonFinderActivity extends PreferenceActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        // setContentView(R.layout.main);
+        addPreferencesFromResource(R.xml.userpreference);
     }
 
     @Override
     protected void onResume() {
         // TODO Auto-generated method stub
         super.onResume();
-        // TODO check the current setting ユーザが設定されているかどうか。
-        Intent intent = new Intent(this, PersonFinderUserPreferenceActivity.class);
-        startActivity(intent);
     }
     
     
